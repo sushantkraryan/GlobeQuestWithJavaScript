b5ab61795d8ce746a761a54bdf9b056eaa9c9df2
Sushant Aryan <sushantkraryan@gmail.com>
2024-08-16 23:23:37 +0530

    Display all the cards

---
+++ script.js
@@ -0,0 +1,23 @@
+const countriesContainer = document.querySelector(".countries-container");
+
+fetch("https://restcountries.com/v3.1/all")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((country) => {
+      console.log(country.name.common);
+      const countryCard = document.createElement("a");
+      countryCard.classList.add("country-card");
+
+      const cardHTML = `
+        <img src="${country.flags.svg}" alt="" />
+        <div class="card-text">
+            <h3 class="card-title">${country.name.common}</h3>
+            <p><b>Population: </b>${country.population.toLocaleString("en-IN")}</p>
+            <p><b>Region: </b>${country.region}</p>
+            <p><b>Capital: </b>${country.capital}</p>
+        </div>`;
+
+      countryCard.innerHTML = cardHTML;
+      countriesContainer.append(countryCard);
+    });
+  });
